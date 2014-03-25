bab50e506f67931bbc0f6157e704fd8b38fa9a7b
uburuntu <bek-roma@ya.ru>
2014-03-25 22:38:29 +0400

    takie dela

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ var jump = -4.6;
 var score = 0;
 var highscore = 0;
 
-var pipeheight = 90;
+var pipeheight = 100;
 var pipewidth = 52;
 var pipes = new Array();
 
@@ -335,8 +335,6 @@ function setMedal()
       medal = "silver";
    if(score >= 30)
       medal = "gold";
-   if(score >= 40)
-      medal = "platinum";
    
    elemmedal.append('<img src="assets/medal_' + medal +'.png" alt="' + medal +'">');
    
